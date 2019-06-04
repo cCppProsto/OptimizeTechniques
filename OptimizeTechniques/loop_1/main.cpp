a8bdcc9cb2cc98fe943afacee38d336013abe07c
cppprosto <yaroslavoleshko@gmail.com>
2019-06-04 13:32:46 +0300

    updates

--- OptimizeTechniques/loop_1/main.cpp
+++ OptimizeTechniques/loop_1/main.cpp
@@ -12,7 +12,7 @@ static void loop_not_optimized(benchmark::State& state)
     int n = array_size;
     for(int i = 0; i < n; i++)
     {
-      array[i] = std::rand() % 1000;
+      array[i] = i*2;
     }
   }
 }
@@ -23,14 +23,33 @@ static void loop_optimized(benchmark::State& state)
   for (auto _ : state)
   {
     int array[array_size];
-    int n = array_size;
-    for(int *ptr = array; n; --n)
+    int i = 0;
+    unsigned long end = reinterpret_cast<unsigned long>(array) + static_cast<unsigned long>(array_size)*sizeof(int);
+    for(int *ptr = array; reinterpret_cast<unsigned long>(ptr) < end; ptr++)
     {
-      *(ptr++) = std::rand() % 1000;
+      *ptr = i * 2;
+      ++i;
     }
   }
 }
 BENCHMARK(loop_optimized);
 
+
+//------------------------------------------------------------------------------
+static void loop_optimized_2(benchmark::State& state)
+{
+  for (auto _ : state)
+  {
+    int array[array_size];
+    int n = array_size;
+    int i = 0;
+    for(int *ptr = array; n ; n--)
+    {
+      *(ptr++) = i * 2;
+      ++i;
+    }
+  }
+}
+BENCHMARK(loop_optimized_2);
 //------------------------------------------------------------------------------
 BENCHMARK_MAIN();
